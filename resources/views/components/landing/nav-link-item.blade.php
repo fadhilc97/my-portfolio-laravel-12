49af3c59ea019ad25286bae2fe960ca86f3bb936
Fadhil <fadhil@incit.org>
2025-11-29 18:57:48 +0700

    Restyle the nav-link-item

--- resources/views/components/landing/nav-link-item.blade.php
+++ resources/views/components/landing/nav-link-item.blade.php
@@ -1,2 +1,3 @@
-<li class="transition cursor-pointer px-3 py-4 hover:bg-secondary md:hover:scale-105 hover:font-medium"><a
+<li><a
+    class="transition cursor-pointer px-3 py-4 hover:bg-secondary md:hover:scale-105 hover:font-medium w-full inline-block"
     href="{{ $href }}">{{ $label }}</a></li>
