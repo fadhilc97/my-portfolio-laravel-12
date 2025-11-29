14d70388d646ad273963f4eea18f1a4a0169673c
Fadhil <fadhil@incit.org>
2025-11-29 18:56:56 +0700

    Rename the right side of home page partials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{ isOpenHamburger: false }" class="relative flex h-screen p-[1.6rem] overflow-hidden">
   @include('partials.landing.home.left-side')
-  @include('partials.landing.home.right-side')
+  @include('partials.landing.home.nav-links')
   @include('partials.landing.home.profile-picture')
 </div>
