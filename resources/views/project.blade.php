e8da3a8d2f01a618fd0dcfa3816ac5c228d33cca
Fadhil <fadhil@incit.org>
2025-12-08 21:16:10 +0700

    Add the project animation slider

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -2,28 +2,60 @@
   $assetBase = asset('');
 @endphp
 
-<div x-data="{
-    assetBase: '{{ $assetBase }}',
-    projects: [{
+@push('style')
+  <style>
+    [x-cloak] {
+      display: none !important;
+    }
+  </style>
+@endpush
+
+<script>
+  function getData() {
+    return {
+      assetBase: '{{ $assetBase }}',
+      currentSlide: 0,
+      prev() {
+        if (this.currentSlide <= 0) {
+          this.currentSlide = this.projects.length - 1;
+          return;
+        }
+        this.currentSlide--;
+      },
+      next() {
+        if (this.currentSlide >= this.projects.length - 1) {
+          this.currentSlide = 0;
+          return;
+        }
+        this.currentSlide++;
+      },
+      projects: [{
+        id: 1,
         imgPath: 'img/projects/daily-savings.png',
         title: 'Daily Savings',
         description: 'Recording and summarize my savings in daily basis.',
         techStacks: ['TypeScript', 'Next.js', 'Tailwind CSS', 'PostgreSQL', 'Prisma.js', 'Recharts']
-    }, {
+      }, {
+        id: 2,
         imgPath: 'img/projects/technical-docs.png',
         title: 'Technical Docs',
         description: 'Simple technical documentation about JavaScript Fundamentals.',
         techStacks: ['HTML', 'CSS']
-    }, {
+      }, {
+        id: 3,
         imgPath: 'img/projects/valorant-info.png',
         title: 'Valorant Info - Asia Pacific 2022',
         description: 'Introduction about game Valorant in general and tournament information in Asia Pacific 2022. This site is written in Bahasa Indonesia.',
         techStacks: ['HTML', 'CSS', 'JavaScript']
-    }]
-}" class="relative h-auto lg:h-screen p-[1.6rem] overflow-hidden">
+      }]
+    }
+  }
+</script>
+
+<div x-data="getData()" x-cloak class="relative h-auto lg:h-screen p-[1.6rem] overflow-hidden">
   <x-landing.nav-control prev-href="/experience" next-href="/blog" />
 
-  <div class="grid grid-cols-1 lg:grid-cols-2">
+  <div class="grid grid-cols-1 lg:grid-cols-2 h-[30vh]">
     <div class="font-stack-sans-notch text-center lg:text-left">
       <h1>Projects</h1>
       <h2>My Showcase</h2>
@@ -36,29 +68,29 @@
     </div>
 
     <div class="py-8 lg:py-16 lg:px-28">
-      <h3 class="text-quatenary text-center">Project title</h3>
-      <p class="text-quatenary/70 text-center mt-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Minus quia
-        aliquid, voluptate
-        quibusdam error ab
-        soluta, blanditiis, quo saepe iusto nostrum. Nobis, labore itaque. Natus beatae voluptate necessitatibus quo
-        facilis.</p>
+      <h3 class="text-quatenary font-medium text-center" x-text="projects[currentSlide].title"></h3>
+      <p class="text-quatenary/90 text-center mt-4 text-sm" x-text="projects[currentSlide].description"></p>
+      <p class="text-quatenary/70 text-center text-sm mt-1">Tech stacks: <span
+          x-text="projects[currentSlide].techStacks.join(', ')"></span></p>
     </div>
   </div>
 
-  <div class="flex lg:flex-col flex-col-reverse gap-4 pb-16">
-    <div class="w-1/2 mx-auto h-[360px] gap-4 overflow-auto flex border rounded-md p-4 border-quatenary">
-      <template x-for="project in projects">
-        <div class="w-full flex-shrink-0 text-white flex justify-center text-3xl h-auto overflow-hidden">
+  <div class="flex lg:flex-col flex-col-reverse gap-4 pb-16 mt-4">
+    <div class="w-1/2 mx-auto h-[360px] overflow-hidden flex border rounded-md p-4 border-quatenary">
+      <template x-for="(project, index) in projects" :key="project.id">
+        <div
+          class="slide-transition w-full flex-shrink-0 text-white flex justify-center text-3xl h-auto overflow-hidden"
+          :style="`transform: translateX(-${currentSlide * 100}%)`">
           <img :src="assetBase + project.imgPath" :alt="project.title" />
         </div>
       </template>
     </div>
 
     <div class="mx-auto w-1/2 gap-8 flex justify-center items-center">
-      <button type="button" class="text-3xl cursor-pointer transition hover:scale-120">&lt;</button>
-      <div class="text-quatenary font-medium text-lg">1 / n</div>
-      <button type="button" class="text-3xl cursor-pointer transition hover:scale-120">&gt;</button>
+      <button @click="prev()" type="button" class="text-3xl cursor-pointer transition hover:scale-120">&lt;</button>
+      <div class="text-quatenary text-center font-medium text-lg w-10"><span x-text="currentSlide + 1"></span> / <span
+          x-text="projects.length"></span></div>
+      <button @click="next()" type="button" class="text-3xl cursor-pointer transition hover:scale-120">&gt;</button>
     </div>
   </div>
 
