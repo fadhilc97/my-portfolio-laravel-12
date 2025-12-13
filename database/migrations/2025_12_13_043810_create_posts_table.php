9757445702e6a6e286315e36808f09168c267309
Fadhil <fadhil@incit.org>
2025-12-13 11:53:49 +0700

    Create Category and Post models

---
+++ database/migrations/2025_12_13_043810_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('slug', 50);
+            $table->string('description', 150)->nullable();
+            $table->text('body');
+            $table->integer('likes')->default(0);
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            $table->foreignId('user_id')->constrained('users', indexName:'post_author_id');
+            $table->foreignId('category_id')->constrained('categories', indexName:'post_category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
