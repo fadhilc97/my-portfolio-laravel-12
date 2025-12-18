67b1bbc40f0804a2c8e19ca2e4b9ef65bb1628ae
Fadhil <fadhil@incit.org>
2025-12-18 22:47:36 +0700

    Finalize the category management

--- app/Livewire/BlogPost/Category.php
+++ app/Livewire/BlogPost/Category.php
@@ -2,12 +2,57 @@
 
 namespace App\Livewire\BlogPost;
 
+use App\Models\Category as ModelsCategory;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Category extends Component
 {
-    public function render()
-    {
-        return view('livewire.blog-post.category');
-    }
+  use WithPagination;
+
+  #[Validate('required|unique:categories|min:3|max:25')]
+  public string $name = '';
+
+  public string $slug = '';
+
+  #[Computed()]
+  public function categories() {
+    return ModelsCategory::paginate(10);
+  }
+
+  public function handleCreate() {
+    $validated = $this->validate();
+
+    ModelsCategory::create([
+      'name' => $validated['name'],
+      'slug' => Str::slug($validated['name'])
+    ]);
+
+    $this->reset();
+  }
+
+  public function handleEdit(int $id) {
+    $validated = $this->validate([
+      'name' => "required|unique:categories,name$id|min:3|max:25"
+    ]);
+
+    ModelsCategory::where('id', $id)->update([
+      'name' => $validated['name'],
+      'slug' => Str::slug($validated['name'])
+    ]);
+
+    $this->reset();
+  }
+
+  public function handleDelete(int $id) {
+    ModelsCategory::find($id)->delete();
+  }
+
+  public function render()
+  {
+    return view('livewire.blog-post.category');
+  }
 }
--- resources/views/livewire/blog-post/category.blade.php
+++ resources/views/livewire/blog-post/category.blade.php
@@ -1,11 +1,20 @@
-<div class="space-y-8">
+<div x-data="{ currentSlugEdit: null }" class="space-y-8">
   <h2>Categories</h2>
-  <flux:button variant="primary" class="cursor-pointer">Create new category</flux:button>
+  <form x-show="!currentSlugEdit" wire:submit.prevent="handleCreate" class="flex items-center gap-2">
+    <flux:field>
+      <flux:input wire:model="name" placeholder="Category Name" type="text" />
+      <flux:error name="name" />
+    </flux:field>
+    <flux:button x-bind:disabled="currentSlugEdit" x-bind:type="currentSlugEdit ? 'submit' : 'button'" variant="primary"
+      class="cursor-pointer">Create new
+      category</flux:button>
+  </form>
+  <h3 x-show="currentSlugEdit">Edit Category</h3>
   <div class="relative overflow-x-auto bg-neutral-primary-soft rounded-base border border-default">
     <table class="w-full text-sm text-left rtl:text-right text-body">
       <thead class="text-sm text-body bg-neutral-secondary-medium border-b border-default-medium">
         <tr>
-          <th scope="col" class="px-6 py-3 font-medium">
+          <th scope="col" class="px-6 py-3 font-medium w-1/3">
             <div class="flex items-center">
               Name
               <button type="button">
@@ -27,18 +36,48 @@
         </tr>
       </thead>
       <tbody>
-        <tr class="bg-neutral-primary-soft border-b  border-default">
-          <td class="px-6 py-4">
-            Category 1
-          </td>
-          <td class="px-6 py-4">
-            15
-          </td>
-          <td class="px-6 py-4 text-primary/50">
-            <a href="#" class="font-medium text-sky-600 hover:underline">Edit</a> |
-            <a href="#" class="font-medium text-red-600 hover:underline">Delete</a>
-          </td>
-        </tr>
+        @forelse($this->categories as $category)
+          <tr wire:key="category-{{ $category->slug }}" class="bg-neutral-primary-soft border-b  border-default">
+            <td class="px-6 py-4">
+              <div x-show="currentSlugEdit !== '{{ $category->slug }}'">
+                {{ $category->name }}
+              </div>
+              <flux:field class="w-auto" x-show="currentSlugEdit === '{{ $category->slug }}'">
+                <flux:input value="{{ $category->name }}" id="category-{{ $category->slug }}" wire:model="name"
+                  placeholder="Category Name" type="text"
+                  x-on:blur="() => {
+                    currentSlugEdit = null;
+                    $wire.handleEdit('{{ $category->id }}');
+                  }"
+                  x-ref="edit-category-{{ $category->slug }}" />
+                <flux:error name="name" />
+              </flux:field>
+            </td>
+            <td class="px-6 py-4">
+              0
+            </td>
+            <td class="px-6 py-4 text-primary/50">
+              <button
+                x-on:click="() => {
+                  currentSlugEdit = '{{ $category->slug }}';
+                  $wire.name = '{{ $category->name }}';
+                  $nextTick(() => {
+                    $refs['edit-category-{{ $category->slug }}'].focus();
+                  });
+                }"
+                type="button" class="cursor-pointer font-medium text-sky-600 hover:underline">Edit</button> |
+              <button wire:click="handleDelete({{ $category->id }})"
+                wire:confirm="Are you sure want to delete this category ?" type="button"
+                class="cursor-pointer font-medium text-red-600 hover:underline">Delete</button>
+            </td>
+          </tr>
+        @empty
+          <tr class="bg-neutral-primary-soft border-b  border-default">
+            <td colspan="3" class="px-6 py-4 text-center">
+              Data empty
+            </td>
+          </tr>
+        @endforelse
       </tbody>
     </table>
   </div>
