ef3a920eba6744d098a79e1bbc18016cb2752eb8
Fadhil <fadhil@incit.org>
2025-12-22 23:05:10 +0700

    Finish the create blog functionality

--- app/Livewire/BlogPost/CreateBlog.php
+++ app/Livewire/BlogPost/CreateBlog.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire\BlogPost;
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateBlog extends Component
 {
-    public function render()
-    {
-        return view('livewire.blog-post.create-blog');
-    }
+  #[Validate('required|min:3|max:50|unique:posts')]
+  public string $title;
+
+  #[Validate('required|min:3|max:500')]
+  public string $description;
+
+  #[Validate('required|integer')]
+  public string|int $category_id = '';
+
+  #[Validate('required')]
+  public string $body;
+
+  public function handleCreate() {
+    $validated = $this->validate();
+    $validated['slug'] = Str::slug($validated['title']);
+    $validated['user_id'] = Auth::user()->id;
+    
+    Post::create($validated);
+
+    return redirect('/app/blog');
+  }
+
+  public function render()
+  {
+    return view('livewire.blog-post.create-blog');
+  }
 }
