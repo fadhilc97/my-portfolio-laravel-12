ea96248a39b58d68db7f55aa826cb1dad8534d84
Fadhil <fadhil@incit.org>
2026-01-09 23:22:07 +0700

    Fix the clickable saweria QR code

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -5,9 +5,12 @@
       <h3 class="text-center">Kindly to support me to make this website useful</h3>
     </div>
     <p class="mt-8 text-center text-lg font-medium">Scan or click the QR code below to donate</p>
-    <a href="https://saweria.co/fadhilc97" target="_blank" class="text-center text-xl block mt-4 text-sky-600 underline">
-      <img src="{{ asset('img/saweria-qr.png') }}" alt="Saweria QR Code" width="250" class="mx-auto" />
-    </a>
-    <a href="/" class="font-medium text-lg mt-4 text-sky-600 text-center block">Back to home</a>
+    <div class="mx-auto flex flex-col items-center">
+      <a href="https://saweria.co/fadhilc97" target="_blank"
+        class="text-center text-xl inline-block mt-4 text-sky-600 underline">
+        <img src="{{ asset('img/saweria-qr.png') }}" alt="Saweria QR Code" width="250" />
+      </a>
+      <a href="/" class="font-medium text-lg mt-4 text-sky-600 text-center block">Back to home</a>
+    </div>
   </div>
 </x-layouts.blog>
