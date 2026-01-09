2fea1de17a3abc5c65ef936ecdaf0a6f71089e10
Fadhil <fadhil@incit.org>
2026-01-09 23:19:12 +0700

    Remove default favicon and adjust the tab title

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,9 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>{{ $title ?? config('app.name') }}</title>
-
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
