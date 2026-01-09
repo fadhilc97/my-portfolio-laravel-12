d082599eba7daefd3ded3c08bbc1d56a7b74b954
Fadhil <fadhil@incit.org>
2026-01-09 22:28:33 +0700

    Add download CV button

--- resources/views/partials/landing/home/left-side.blade.php
+++ resources/views/partials/landing/home/left-side.blade.php
@@ -8,4 +8,10 @@
   <p class="font-inter mt-4 leading-8 text-xl text-quatenary/70 md:w-2/3">I'm a problem solver and tech enthusiast
     based
     in Indonesia. Passionate to help your business problem with digital solution. </p>
+
+  {{-- <flux:button href="{{ asset('cv.pdf') }}" variant="primary" class="mt-4 cursor-pointer">Download CV</flux:button> --}}
+
+  <a href="{{ asset('cv.pdf') }}" target="_blank"
+    class="py-2 px-4 hover:text-primary hover:bg-quatenary transition duration-300 inline-block font-inter text-quatenary/70 mt-4 border border-quatenary/70 cursor-pointer">Download
+    CV</a>
 </div>
