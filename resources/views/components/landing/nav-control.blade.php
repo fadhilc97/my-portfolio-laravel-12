0001116d07e666033102dada4c9c234fe623fa06
Fadhil <fadhil@incit.org>
2026-01-13 21:27:44 +0700

    Fix not clickable the navigation control

--- resources/views/components/landing/nav-control.blade.php
+++ resources/views/components/landing/nav-control.blade.php
@@ -1,5 +1,5 @@
 <nav
-  class="fixed bottom-[1.6rem] right-1/2 translate-x-1/2 md:right-[1.6rem] md:translate-x-0 md:bottom-auto bg-secondary px-4 py-1 rounded-full">
+  class="fixed bottom-[1.6rem] right-1/2 translate-x-1/2 md:right-[1.6rem] md:translate-x-0 md:bottom-auto md:bg-secondary bg-secondary/75 px-4 py-1 rounded-full z-50">
   <ul class="flex gap-6 items-center text-4xl text-quatenary/80">
     <li @class([
         'transition hover:text-quatenary md:hover:scale-110 hover:font-medium' => $prevHref,
