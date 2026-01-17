229afe408ada1c3af273cca17fb36bfbfa8a47df
Fadhil <fadhil@incit.org>
2026-01-17 16:55:59 +0700

    Implement upload cover image when update the blog post

--- app/Livewire/BlogPost/EditBlog.php
+++ app/Livewire/BlogPost/EditBlog.php
@@ -6,9 +6,13 @@ use App\Models\Post;
 use App\Data\PostData;
 use App\Interface\BlogServiceInterface;
 use Livewire\Component;
+use Spatie\LivewireFilepond\WithFilePond;
+use Livewire\Attributes\Validate;
 
 class EditBlog extends Component
 {
+  use WithFilePond;
+
   public string $slug;
 
   public string $title;
@@ -21,6 +25,11 @@ class EditBlog extends Component
 
   public string $status;
 
+  public string $cover_url;
+
+  #[Validate('max:5120')]
+  public $cover_image_file;
+
   public function mount($slug) {
     $postQuery = Post::where('slug', $slug)->first();
     $postData = PostData::fromModel($postQuery);
@@ -31,6 +40,7 @@ class EditBlog extends Component
     $this->body = $postData->body;
     $this->slug = $slug;
     $this->status = $postData->status;
+    $this->cover_url = $postData->cover_url ?? asset('img/default-cover.jpg');
   }
 
   public function handleSubmit() {
@@ -38,13 +48,20 @@ class EditBlog extends Component
       'title' => 'required|min:3|max:50|unique:posts,title'.$this->slug,
       'description' => 'required|min:3|max:500',
       'category_id' => 'required|integer|min:1',
-      'body' =>  'required'
+      'body' =>  'required',
     ], [
       '*.required' => "Required",
       'category_id.min' => "Please choose category"
     ]);
 
-    Post::where('slug', $this->slug)->update($validated);
+    $post = Post::where('slug', $this->slug)->firstOrFail();
+    $post->update($validated);
+
+    if ($this->cover_image_file) {
+      $post->clearMediaCollection('cover')->addMedia($this->cover_image_file->getRealPath())
+        ->usingFileName($this->cover_image_file->getClientOriginalName())
+        ->toMediaCollection('cover');
+    }
 
     return redirect('/app/blog');
   }
--- resources/views/components/blog-app/form-post.blade.php
+++ resources/views/components/blog-app/form-post.blade.php
@@ -1,5 +1,6 @@
 @assets
   <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet" />
+  @filepondScripts
 @endassets
 
 <div class="space-y-8">
@@ -25,6 +26,13 @@
         @endforeach
       </flux:select>
       <flux:error name="category_id" />
+    </flux:field>
+
+    <flux:field>
+      <flux:label badge="Required">Cover Image</flux:label>
+      <x-filepond::upload wire:model="cover_image_file" accepted-file-types="image/*" />
+      <img src="{{ $this->cover_url }}" alt="{{ $this->title }}" class="h-40 mx-auto w-full object-cover mb-8" />
+      <flux:error name="cover_image_file" />
     </flux:field>
 
     <flux:field>
